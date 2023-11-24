b19b243d1c25aaf83a5060888d80600824b77d5e
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2023-11-24 02:32:16 -0300

    chore: insert time on store

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,7 +4,8 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/ProductCard.html': path.resolve(__dirname, 'template/js/custom-js/html/ProductCard.html'),
-      './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductCard.js')
+      './js/ProductCard.js': path.resolve(__dirname, 'template/js/custom-js/js/ProductCard.js'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js')
     }
   }
 })
